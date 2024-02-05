40f960b757da3474077fbfcccabb54b98aeff9eb
juvv9 <jun.nakama1030@gmail.com>
2024-02-05 23:37:33 +0900

    アプリケーションにテスト用の家を作成

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -23,4 +23,17 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
+
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
+
 }
